ec8e15591e7b70b28ebf993b7aa6e5689086d848
Eugene Lazutkin <eugene@lazutkin.com>
2014-05-27 17:41:02 -0500

    Renamed main.js to ice.js, added volo-specific section, and bumped the version.

--- console.js
+++ console.js
@@ -1,5 +1,5 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["./main"], function(ice){
+(["./ice"], function(ice){
 	"use strict";
 
 	function format(args){
--- tests/simulator.js
+++ tests/simulator.js
@@ -1,5 +1,5 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["module", "../main"], function(module, ice){
+(["module", "../ice"], function(module, ice){
 	"use strict";
 
 	var ice = ice.specialize(module);
