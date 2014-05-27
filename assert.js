2ee3cc6409cdc5c4290054c0dfbf7890889a2b6f
Eugene Lazutkin <eugene@lazutkin.com>
2014-05-27 18:09:07 -0500

    Renamed back to main.js to accommodate requirejs.

--- assert.js
+++ assert.js
@@ -1,5 +1,5 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["./ice"], function(ice){
+(["./main"], function(ice){
 	"use strict";
 
 	function quoteString(text){
